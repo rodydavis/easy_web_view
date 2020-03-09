91a97dc20b212c88d2bb828434e40784ea96e17b
Rody Davis <rody.davis.jr@gmail.com>
2020-03-08 21:05:48 -0700

    updating example

---
+++ docs/flutter_service_worker.js
@@ -0,0 +1,36 @@
+'use strict';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "/index.html": "00fa4d0e5c5956ceb97755d9981ce3d0",
+"/main.dart.js": "109f503b0f6a318f54cb21aef1124828",
+"/assets/LICENSE": "64fadb4d3cef428d96710493624a5c32",
+"/assets/AssetManifest.json": "0d266ffbe90dae02458487c9d33b7373",
+"/assets/FontManifest.json": "f7161631e25fbd47f3180eae84053a51",
+"/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "9a62a954b81a1ad45a58b9bcea89b50b",
+"/assets/packages/flutter_markdown/assets/logo.png": "67642a0b80f3d50277c44cde8f450e50",
+"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
+};
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheName) {
+      return caches.delete(cacheName);
+    }).then(function (_) {
+      return caches.open(CACHE_NAME);
+    }).then(function (cache) {
+      return cache.addAll(Object.keys(RESOURCES));
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
