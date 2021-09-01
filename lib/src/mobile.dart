c8a6824877da90827f919432cce6f4052898b3cc
Sébastien Dabet <sebastien.dabet@orpheo.fr>
2021-09-01 10:11:30 +0200

    Add 'allow' parameter for iframes

--- lib/src/mobile.dart
+++ lib/src/mobile.dart
@@ -14,6 +14,7 @@ class EasyWebView extends StatefulWidget implements EasyWebViewImpl {
     this.height,
     this.width,
     this.webAllowFullScreen = true,
+    this.allow = "",
     this.isHtml = false,
     this.isMarkdown = false,
     this.convertToWidgets = false,
@@ -38,6 +39,9 @@ class EasyWebView extends StatefulWidget implements EasyWebViewImpl {
 
   @override
   final bool webAllowFullScreen;
+
+  @override
+  final String allow;
 
   @override
   final bool isMarkdown;
