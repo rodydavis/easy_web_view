c951bb19dcdee38856bcd06482e2e8d79a1203f8
rodydavis <rodydavis@users.noreply.github.com>
2022-04-05 17:19:11 +0000

    deploy: f6e37389b0202a430e6f66609b8b9fd07046ef2a

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,18 +3,23 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "version.json": "4b6db237b3514a88107a422469adfb0f",
-"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"main.dart.js": "099f26b5fb560d6097880fa7af1c15b2",
+  "manifest.json": "15f73b7e8a8209c2206210b3ac8dea1b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"manifest.json": "15f73b7e8a8209c2206210b3ac8dea1b",
-"assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
-"assets/NOTICES": "209403b86e535aa681d252946d9052f7",
-"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
+"version.json": "ff966ab969ba381b900e61629bfb9789",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
+"assets/assets/test_html_to_pdf.html": "5124b01973f80a651744715037e32d05",
+"assets/AssetManifest.json": "f5ce82c4cf9863ec2c297a470774dd7d",
+"assets/fonts/MaterialIcons-Regular.otf": "7e7a6cccddf6d7b20012a548461d5d81",
+"assets/NOTICES": "580ba18622d1ab573030a85d31eb2f83",
+"canvaskit/canvaskit.js": "f00de1f742223b7cf4cec1c2a0cd9764",
+"canvaskit/canvaskit.wasm": "efe4a5da0205bb8d8c5aca7dad981abd",
+"canvaskit/profiling/canvaskit.js": "411ee45f5abb57975ee5243310c6953e",
+"canvaskit/profiling/canvaskit.wasm": "c6681b1a749ad902eefcba11fed1cb3f",
 "index.html": "00fa4d0e5c5956ceb97755d9981ce3d0",
-"/": "00fa4d0e5c5956ceb97755d9981ce3d0"
+"/": "00fa4d0e5c5956ceb97755d9981ce3d0",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"main.dart.js": "c39be998675b3b35640efba37d6986b1"
 };
 
 // The application shell files that are downloaded before a service worker can
