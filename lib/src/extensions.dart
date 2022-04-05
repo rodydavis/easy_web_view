766f3fc85638c8c271bd48e70f4dc773484c81c4
Rody Davis <rody.davis.jr@gmail.com>
2022-04-05 12:26:25 -0700

    updating extensions

--- lib/src/extensions.dart
+++ lib/src/extensions.dart
@@ -3,8 +3,10 @@ import 'package:markdown/markdown.dart' as md;
 
 extension StringUtils on String {
   bool get isValidHtml => this.contains('<html>') && this.contains('</html>');
+  
   bool get isValidUrl =>
       this.startsWith('https://') || this.startsWith('http://');
+
   bool get isValidMarkdown => !isValidUrl && !isValidHtml;
 
   String get dataUrl {
@@ -14,26 +16,32 @@ extension StringUtils on String {
     }
     if (isValidHtml) {
       _src =
-          "data:text/html;charset=utf-8,${Uri.encodeComponent(this.wrappedHtml)}";
+          "data:text/html;charset=utf-8,${Uri.encodeComponent(this.wrapHtml())}";
     }
     return _src;
   }
 
   String get markdown => html2md.convert(this);
+
   String get html => md.markdownToHtml(this);
-  String get wrappedHtml {
-    return """
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-${this}
-</body>
-</html>
-  """;
+
+  String wrapHtml({
+    String title = 'Document',
+    String pad = '    ',
+  }) {
+    final sb = StringBuffer();
+    final add = (String s) => sb.writeln(s);
+    add('<!DOCTYPE html>');
+    add('<html lang="en">');
+    add('<head>');
+    add('$pad<meta charset="UTF-8">');
+    add('$pad<meta name="viewport" content="width=device-width, initial-scale=1.0">');
+    add('$pad<title>${title}</title>');
+    add('</head>');
+    add('<body>');
+    add(this);
+    add('</body>');
+    add('</html>');
+    return sb.toString();
   }
 }
